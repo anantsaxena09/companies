6dddb79070e5fdccb3e75209590db07b483077f9
Anant Saxena <anantsaxena1990@gmail.com>
2024-09-01 19:01:23 +0530

    main

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import connectMongo from "../../../lib/mongodb";
-import User from "../../../models/User";
+import User from "../../../models/user";
 
 export const authOptions = {
   providers: [
