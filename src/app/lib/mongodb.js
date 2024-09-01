3f6b057615491a1b695f8558a2fbc9aac7b4f048
Anant Saxena <anantsaxena1990@gmail.com>
2024-09-02 02:30:10 +0530

    Error handling

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -4,10 +4,17 @@ const connectMongo = async () => {
   if (mongoose.connections[0].readyState) {
     return;
   }
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    throw new Error("Database connection error");
+  }
 };
 
 export default connectMongo;
